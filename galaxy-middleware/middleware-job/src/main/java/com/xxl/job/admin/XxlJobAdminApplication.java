16aaf56d02dcc53583482c0f602637fafedb5ac0
admin <admin>
2019-11-14 09:26:54 +0800

    sdffgdfdgdfgdf

--- galaxy-middleware/middleware-job/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ galaxy-middleware/middleware-job/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -2,12 +2,11 @@ package com.xxl.job.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
-@SpringCloudApplication
+@SpringBootApplication
 public class XxlJobAdminApplication {
 
 	public static void main(String[] args) {
