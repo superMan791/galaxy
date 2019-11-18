0e5c87c89159bbb5c6c4155da3f0208da691d2d2
superMan791 <1439097053@qq.com>
2019-11-18 21:30:19 +0800

    sdf

--- galaxy-demo/demo-db/src/main/java/com/cloud/galaxy/demo/jooq/controller/DemoController.java
+++ galaxy-demo/demo-db/src/main/java/com/cloud/galaxy/demo/jooq/controller/DemoController.java
@@ -5,8 +5,13 @@ import com.cloud.galaxy.demo.jooq.db.gen.tables.SysRole;
 import com.cloud.galaxy.demo.jooq.db.gen.tables.SysUser;
 import com.cloud.galaxy.demo.jooq.db.gen.tables.records.SysUserRecord;
 import com.cloud.galaxy.demo.jooq.entity.SysUserPo;
+import org.jooq.Condition;
 import org.jooq.DSLContext;
+
 import static org.jooq.impl.DSL.*;
+
+import org.jooq.Record;
+import org.jooq.SelectQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +21,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -100,5 +106,20 @@ public class DemoController {
                     Integer count = (Integer) record.get("idCount");
                     return count;
                 }).collect(Collectors.toList());
+    }
+
+    public List<SysUserPo> selectList2(Long id) {
+        SelectQuery<Record> query = dslContext.select().from(sysUser).getQuery();
+        Optional.ofNullable(id).ifPresent(x -> {
+            query.addConditions(sysUser.ID.eq(x));
+        });
+        List<SysUserPo> list = query.fetch().map(u -> {
+            SysUserPo sysUserPo = new SysUserPo();
+            sysUserPo.setId(u.get(sysUser.ID));
+            sysUserPo.setName(u.get(sysUser.NAME));
+            sysUserPo.setBirth(u.get(sysUser.BIRTH));
+            return sysUserPo;
+        });
+        return list;
     }
 }
